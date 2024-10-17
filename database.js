1d39dc1129a0990a9469a30fec8b85d114fdea0e
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-10-17 16:21:19 -0300

    feat: conectando com banco de dados e definindo rota POST

--- database.js
+++ database.js
@@ -1,10 +1,18 @@
+import {sql} from './dbNeon.js'
+import randomUUID from 'crypto'
+
 export class DatabasePostgres{
   async list () {
 
   } 
 
-  async create () {
+  async create (video) {
+    const gameId = randomUUID()
+    const {title, description, imgUrl, qtdPlayers, price} = video
 
+    await sql`
+      insert into games (id, title, description, imgUrl, qtdPlayers, price) VALUES (${gameId}, ${title},${description},${imgUrl}, ${qtdPlayers} ${price})
+    `
   }
 
   async update() {
--- server.js
+++ server.js
@@ -1,19 +1,26 @@
-import fastify from "fastify";
+import {fastify} from 'fastify'
+import {DatabasePostgres} from './database.js'
 
 const server = fastify();
+const database = new DatabasePostgres
 
-server.post('', async (req, res) => {
-
-})
-
-server.get('', async (req, res) => {
+server.post('/games', async (req, res) => {
 
 })
 
-server.put('', async (req, res) => {
+server.get('/games', async (req, res) => {
 
 })
 
-server.delete('', async (req, res) => {
-  
+server.put('/games/:title', async (req, res) => {
+
+})
+
+server.delete('/games/:title', async (req, res) => {
+
+})
+
+server.listen({
+  host: '0.0.0.0',
+  port: process.env.PORT ?? 3000,
 })
